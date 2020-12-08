a6484e3d2531cb8af1fbcae4a5a5bf6cb9a9e6d9
unknown <teo.sitta@gmail.com>
2020-12-08 16:53:46 -0300

    criando a estrutura do layout e avisos

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 //import
 const express = require('express')
 const bodyParser = require('body-parser')
+
+//importando as rotas do aviso
+const routerAvisos = require('./components/avisos/avisosController')
+
 //ligando o express
 const app = express()
+
 //config view engine
 app.set('view engine','ejs')
 app.use(express.static('public'))
+
 //config bodyparser
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+
 //rotas
-app.get("/",(req,res)=>{
-    res.send("Vai tadeuzord!")
-})
+app.use(routerAvisos)
+
 //config da porta
 app.listen(3000)
