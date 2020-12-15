493d660c3a6a6d677b62598ca3e4e080ca54757f
unknown <teo.sitta@gmail.com>
2020-12-15 19:42:01 -0300

    selecionar um aviso e alterar

--- components/avisos/avisos.js
+++ components/avisos/avisos.js
@@ -18,6 +18,21 @@ return BD.insert(aviso).into('avisos')
 
     }) 
 }
+
+function editar(aviso, id){
+    return BD('avisos').where('ID_avisos', id).update(aviso)
+    .then( _ =>{
+
+        return { tipo: 'sucesso', corpo: 'Dados alterados com sucesso'}
+
+    })
+    .catch(erro =>{
+
+        return { tipo: 'erro', corpo: 'Erro ao alterar dados'}
+
+    }) 
+}
+
 /** 
 * @returns {object} 
 */
@@ -37,6 +52,20 @@ function selecionartudo(){
 
 /**
  * 
+ * @param {*} id
+ * @returns {object} 
+ */
+function selecionarAviso(id){
+    return BD.select('*').from('avisos').where('ID_avisos', id).first()
+        .then(aviso => {return aviso})
+        .catch(erro => {
+            return { tipo: "erro", corpo: "Erro: " + erro}
+        })
+
+}
+
+/**
+ * 
  * @param {int} id
  */
 
@@ -44,4 +73,4 @@ function excluir(id){
     return BD.del().from('avisos').where('ID_avisos',id)
 }
 
-module.exports = {salvar,selecionartudo,excluir}
+module.exports = {salvar,selecionartudo,excluir,selecionarAviso,editar}
--- components/avisos/avisosController.js
+++ components/avisos/avisosController.js
@@ -15,17 +15,36 @@ router.get("/avisos/novo",(req,res)=>{
     res.render('formulario_avisos')
 })
 
+router.get("/avisos/editar/:id", async (req,res)=>{
+    const id = req.params.id
+    const aviso = await Avisos.selecionarAviso(id)
+    res.render('formulario_avisos',{aviso})
+})
+
 router.post('/avisos/novo', async (req,res)=>{
     const titulo = req.body.titulo
     const data = req.body.data
     const Mensagem = req.body.Mensagem
 
     const msg = await Avisos.salvar({titulo,data,Mensagem})
-    console.log(msg)
     res.render('formulario_avisos',{msg})
 })
 
-router.get("avisos/excluir/:id", async (req,res)=>{
+router.post("/avisos/editar/:id", async (req,res) =>{
+    const id = req.body.id
+    const titulo = req.body.titulo
+    const data = req.body.data
+    const Mensagem = req.body.Mensagem
+
+    const msg = await Avisos.editar({titulo,data,Mensagem}, id)
+    if(msg.tipo === "sucesso"){
+        res.redirect('/avisos')
+    }else{
+        res.render('formulario_avisos',{msg})
+    }
+})
+
+router.get("/avisos/excluir/:id", async (req,res)=>{
     const id = Number(req.params.id)
     await Avisos.excluir(id)
     res.redirect('/avisos')
